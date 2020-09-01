4111a64ae64fac0c8c8fce7b2943e85bda0ade57
aaryapatel <aarya.patel@mail.utoronto.ca>
2020-08-31 20:08:51 -0400

    Updates

---
+++ precache-manifest.dea5a20928b5fbff3fb4f8ae17b5be1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1aff1fed97d3c8e16b45c1bb2accb0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d167c7012848c1766121",
+    "url": "/static/css/main.fae657e1.chunk.css"
+  },
+  {
+    "revision": "633c39361f8f0af2854c",
+    "url": "/static/js/2.69f131ea.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.69f131ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d167c7012848c1766121",
+    "url": "/static/js/main.7dacd421.chunk.js"
+  },
+  {
+    "revision": "e5d1fe3fab80aab9cc15",
+    "url": "/static/js/runtime-main.c2e59deb.js"
+  }
+]);
